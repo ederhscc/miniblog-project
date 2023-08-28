e3451037af2e72afb5c3c14d280cca8e60e0de90
Eder Henrique <ederhscc@gmail.com>
2023-08-28 14:57:14 -0300

    seting React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
 
 function App() {
-  
   return (
     <>
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
