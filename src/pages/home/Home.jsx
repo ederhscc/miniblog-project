15352d0982da4a87939a0b7de6b5ae1520355b9f
Eder Henrique <ederhscc@gmail.com>
2023-09-01 13:53:18 -0300

    added search funcionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,14 +3,20 @@ import styles from "./Home.module.css";
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 import PostDetail from "../../components/PostDetail";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const {documents: posts, loading} = useFetchDocuments("posts"); 
+  const { documents: posts, loading } = useFetchDocuments("posts");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
   };
 
   return (
@@ -27,13 +33,13 @@ const Home = () => {
       </form>
       <div>
         {loading && <p>Carregando...</p>}
-        {posts && posts.map((post) => (
-            <PostDetail key={post.id} post={post}/>
-        ))}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts.</p>
-            <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
           </div>
         )}
       </div>
